a36d414c2a43074f0e0769986d02bfbcbeb7e312
Dante Sanabria <dante.sanabria@dexcom.com>
2022-09-01 22:11:35 -0700

    update app

--- app/src/test/java/com/example/comicdev/ExampleUnitTest.kt
+++ app/src/test/java/com/example/comicdev/ExampleUnitTest.kt
@@ -14,4 +14,18 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun mutableList(){
+        val numbers = mutableListOf("one", "two", "three", "four")
+        numbers.add("five")
+        assertEquals(5, numbers.size)
+    }
+
+    @Test
+    fun collectionList(){
+        val stringList = listOf("one", "two", "one")
+        val stringSet = setOf("one", "two", "three")
+        for(s in stringList) assertEquals(true, stringSet.contains(s))
+    }
 }
